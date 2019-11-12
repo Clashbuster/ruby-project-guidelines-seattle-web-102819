9c984167dcfb5dd392f389e11e70ea6fa8d734ab
github username <github email address>
2019-11-12 15:31:58 -0800

    clashbuster

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191112230209) do
-
-  create_table "users", force: :cascade do |t|
-    t.string  "name"
-    t.integer "age"
-    t.integer "number_of_propositions"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
